0f01a467a3895fa36d3a08b9e0e03ae3c6965289
Thiago Barcelos <thiagolbf@hotmail.com>
2022-09-05 22:56:59 -0300

    deploy heroku

---
+++ server.js
@@ -0,0 +1,24 @@
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const cors = require("cors");
+const port = process.env.PORT || 3001;
+
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+app.db = router.db;
+
+const rules = auth.rewriter({
+  users: 644,
+  products: 644,
+  whishlist: 600,
+  cupons: 640,
+});
+
+app.use(cors());
+app.use(rules);
+app.use(auth);
+app.use(router);
+app.listen(port);
+
+console.log("Server is running on port:", port);
